5391fcb15d2d9ca085a81816a38e27a52f6d391b
samuL12 <97015477+samuL12@users.noreply.github.com>
2023-07-31 12:10:01 +0900

    [level 0] Title: 짝수의 합, Time: 0.01 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.java
@@ -1,20 +1,6 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        int l = 0;
-        l = n/2; // 등차 항 갯수
-        if(l == 0){
-            answer = 0;
-        }
-        else if(n == 1){
-            answer = 2;
-        }
-        else{
-            if(n%2 == 1){
-                n += -1;
-            }
-            answer = (l*(2 + n))/2;
-        }
         return answer;
     }
 }
