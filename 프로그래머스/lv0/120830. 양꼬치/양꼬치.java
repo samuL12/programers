4eaef8dcdb375ba89ed7fb7d81621c9e83bbaf5c
samuL12 <97015477+samuL12@users.noreply.github.com>
2023-08-02 11:14:26 +0900

    [level 0] Title: 양꼬치, Time: 0.01 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        int service = 0;
+        if(n>10)
+            service = n/10;
+        answer = n*12000 + (k-service)*2000;
+        return answer;
+    }
+}
