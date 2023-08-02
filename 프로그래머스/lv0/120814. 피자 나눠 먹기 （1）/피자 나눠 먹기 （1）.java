b3d3687d39cf762cb5d845da3b79903b97681817
samuL12 <97015477+samuL12@users.noreply.github.com>
2023-08-02 22:43:55 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.02 ms, Memory: 69.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -2,11 +2,14 @@ class Solution {
     public int solution(int n) {
         int answer = 0; // 피자 갯수
         if(n>7){
+            // 피자 갯수 딱 맞아 떨어질 때
             if(n%7 == 0)
                 answer = n/7;
+            // 피자 조각 1개에서 6조각 부족할 때
             else
                 answer = n/7+1;
         }
+        // 사람 7명 미만
         else{
             answer = 1;
         }
