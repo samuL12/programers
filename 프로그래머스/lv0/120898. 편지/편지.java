dbf45c7117d7f346e54d4aee85c95d3070254d87
samuL12 <97015477+samuL12@users.noreply.github.com>
2023-08-02 11:11:15 +0900

    [level 0] Title: 편지, Time: 0.01 ms, Memory: 71.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120898. 편지/편지.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String message) {
+        int total = 0;
+        int answer = 0;
+        total = message.length();
+        answer = total*2;
+        return answer;
+    }
+}
