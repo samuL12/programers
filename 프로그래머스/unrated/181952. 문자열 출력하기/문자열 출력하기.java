c5a644f33e02886a13bdfdda43071a316fc2f6cf
samuL12 <97015477+samuL12@users.noreply.github.com>
2023-08-03 21:29:14 +0900

    Update 문자열 출력하기.java

--- 프로그래머스/unrated/181952. 문자열 출력하기/문자열 출력하기.java
+++ 프로그래머스/unrated/181952. 문자열 출력하기/문자열 출력하기.java
@@ -4,6 +4,7 @@ public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
+        sc.close();
         System.out.println(a);
     }
 }
