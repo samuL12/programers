1ac15674f0747f0c5246839c74a5eee6d98ed19c
samuL12 <97015477+samuL12@users.noreply.github.com>
2023-08-06 02:47:46 +0900

    Update 특정 문자 제거하기.java

--- 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -2,8 +2,9 @@ class Solution {
     public String solution(String my_string, String letter) {
         // letter.length() is 1 
         String answer = "";
+        char c = letter.charAt(0);
         for(int i=0; i<my_string.length(); i++){
-            if(letter.charAt(0) != my_string.charAt(i)){
+            if(c != my_string.charAt(i)){
                 answer += my_string.charAt(i);
             }
         }
