5bb9377b4616fabb7a99606bd6f89b11691061dc
samuL12 <97015477+samuL12@users.noreply.github.com>
2023-08-07 12:43:34 +0900

    Update 문자열을 정수로 변환하기.java

--- 프로그래머스/unrated/181848. 문자열을 정수로 변환하기/문자열을 정수로 변환하기.java
+++ 프로그래머스/unrated/181848. 문자열을 정수로 변환하기/문자열을 정수로 변환하기.java
@@ -9,3 +9,23 @@ class Solution {
         return answer;
     }
 }
+
+/*
+class Solution {
+    public int solution(String n_str) {
+        int answer = 0;
+        for(int i=0; i<n_str.length(); i++){
+            answer = answer * 10;
+            answer += n_str.charAt(i)-48;
+        }
+        return answer;
+    }
+}
+
+class Solution {
+    public int solution(String n_str) {
+        int answer =Integer.parseInt(n_str);
+        return answer;
+    }
+}
+*/
